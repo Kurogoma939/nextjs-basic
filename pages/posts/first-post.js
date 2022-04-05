3eeb0dd17565a5063b5719ba0820c2e3f4e7b54c
kurogoma939 <kan.sss.939@gmail.com>
2022-04-05 23:18:33 +0900

    Script処理

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,6 +2,8 @@
 import Link from 'next/link'
 // メタデータ設定
 import Head from 'next/head'
+// script処理
+import Script from 'next/script'
 
 export default function FirstPost() {
 	return (
@@ -10,11 +12,13 @@ export default function FirstPost() {
 				<title>First Post</title>
 			</Head>
 			<h1>First Post</h1>
-			<h2>
-				<Link href="/">
-					<a>Back to home</a>
-				</Link>
-			</h2>
+			<Script
+				src="https://connect.facebook.net/en_US/sdk.js"
+				strategy="lazyOnload"
+				onLoad={() =>
+					console.log(`script loaded correctly, window.FB has been populated`)
+				}
+			/>
 		</>
 	)
 }
